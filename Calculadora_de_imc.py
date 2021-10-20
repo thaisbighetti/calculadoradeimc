ca80ba83d2ff97a6b8dacfc0f994f69860d4e334
Thais Bighetti <thais.bighetti@hotmail.com>
2021-10-20 18:18:06 -0300

    Update Calculadora_de_imc.py

--- Calculadora_de_imc.py
+++ Calculadora_de_imc.py
@@ -11,13 +11,10 @@ elif imc < 24.9 and isinstance(imc, float):
     print('Seu imc é:', "%.2f" % imc , "(Normal)")
 elif imc < 29.9 and isinstance(imc, float):
     print('Seu imc é:', "%.2f" % imc , "(Sobrepeso)")
-<<<<<<< HEAD
 elif imc < 39.9 and isinstance(imc, float):
     print('Seu imc é:', "%.2f" % imc , "(Obesidade)")
-=======
 elif (imc > 30) and (imc < 39.9):
      print('Seu imc é:', "%.2f" % imc , "(Obesidade)")
->>>>>>> 8de790e22e45fbaecad5abc3368bcd02e87cde9c
 else:
     print('Seu imc é:', "%.2f" % imc , "(Obesidade Grave)")
 
