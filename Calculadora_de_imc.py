4f69a24634d77fcca3f2a08c6e2f66500c983f41
Thais Benevides Bighetti Mazeto <thais.bighetti@hotmail.com>
2021-10-20 17:43:53 -0300

    Calculadora com melhorias

--- Calculadora_de_imc.py
+++ Calculadora_de_imc.py
@@ -1,6 +1,6 @@
 #Calcular IMC
 
-peso=float(input('Qual é o seu peso?')) 
+peso= float(input('Qual é o seu peso?')) 
 altura = float(input ('Qual é sua altura? (Ex:1.90)'))
 
 imc = (peso/altura**2) 
@@ -11,8 +11,13 @@ elif imc < 24.9 and isinstance(imc, float):
     print('Seu imc é:', "%.2f" % imc , "(Normal)")
 elif imc < 29.9 and isinstance(imc, float):
     print('Seu imc é:', "%.2f" % imc , "(Sobrepeso)")
+<<<<<<< HEAD
 elif imc < 39.9 and isinstance(imc, float):
     print('Seu imc é:', "%.2f" % imc , "(Obesidade)")
+=======
+elif (imc > 30) and (imc < 39.9):
+     print('Seu imc é:', "%.2f" % imc , "(Obesidade)")
+>>>>>>> 8de790e22e45fbaecad5abc3368bcd02e87cde9c
 else:
     print('Seu imc é:', "%.2f" % imc , "(Obesidade Grave)")
 
